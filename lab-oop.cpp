828799e14d8dc0ac256a7321c7538fdbcae8b032
Ahmad-Yousaf123 <ahmad.yousaf786@gmail.com>
2016-05-15 09:54:49 +0500

    Testing Github
    It is just about the basic of ATM.

---
+++ lab-oop.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#inlude<fstream>
+#inlude<conio.h>
+using namespace std;
+
+class ATM{
+	private:
+		double balance;
+		double withdraw;
+		double deposit;
+	public:
+			
+	
+	
+};
+
+class trans{
+	private:
+		string bill_name;
+		double bill_amount;
+	public:
+			
+		
+	
+};
+
+class user{
+	private:
+		string user_name;
+		string password;
+	public:
+	
+};
+
